c1f7d25e9df0763e895c30ba8f33062025beff15
Abdelrhman Hany <mindenslavement@gmail.com>
2021-07-10 18:16:12 +0200

    Cut Copy Paste Undo Redo

--- XML_Editor_Project/mainwindow.cpp
+++ XML_Editor_Project/mainwindow.cpp
@@ -6,10 +6,65 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
 }
 
--- build-XML_Editor_Project-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-XML_Editor_Project-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -22,17 +22,32 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    const uint offsetsAndSize[2];
-    char stringdata0[11];
+    const uint offsetsAndSize[22];
+    char stringdata0[230];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-QT_MOC_LITERAL(0, 10) // "MainWindow"
+QT_MOC_LITERAL(0, 10), // "MainWindow"
+QT_MOC_LITERAL(11, 22), // "on_actionNew_triggered"
+QT_MOC_LITERAL(34, 0), // ""
+QT_MOC_LITERAL(35, 23), // "on_actionOpen_triggered"
+QT_MOC_LITERAL(59, 23), // "on_actionSave_triggered"
+QT_MOC_LITERAL(83, 26), // "on_actionSave_As_triggered"
+QT_MOC_LITERAL(110, 22), // "on_actionCut_triggered"
+QT_MOC_LITERAL(133, 23), // "on_actionCopy_triggered"
+QT_MOC_LITERAL(157, 24), // "on_actionPaste_triggered"
+QT_MOC_LITERAL(182, 23), // "on_actionUndo_triggered"
+QT_MOC_LITERAL(206, 23) // "on_actionRedo_triggered"
 
     },
-    "MainWindow"
+    "MainWindow\0on_actionNew_triggered\0\0"
+    "on_actionOpen_triggered\0on_actionSave_triggered\0"
+    "on_actionSave_As_triggered\0"
+    "on_actionCut_triggered\0on_actionCopy_triggered\0"
+    "on_actionPaste_triggered\0"
+    "on_actionUndo_triggered\0on_actionRedo_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -42,21 +57,56 @@ static const uint qt_meta_data_MainWindow[] = {
        9,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       9,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   68,    2, 0x08,    0 /* Private */,
+       3,    0,   69,    2, 0x08,    1 /* Private */,
+       4,    0,   70,    2, 0x08,    2 /* Private */,
+       5,    0,   71,    2, 0x08,    3 /* Private */,
+       6,    0,   72,    2, 0x08,    4 /* Private */,
+       7,    0,   73,    2, 0x08,    5 /* Private */,
+       8,    0,   74,    2, 0x08,    6 /* Private */,
+       9,    0,   75,    2, 0x08,    7 /* Private */,
+      10,    0,   76,    2, 0x08,    8 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_actionNew_triggered(); break;
+        case 1: _t->on_actionOpen_triggered(); break;
+        case 2: _t->on_actionSave_triggered(); break;
+        case 3: _t->on_actionSave_As_triggered(); break;
+        case 4: _t->on_actionCut_triggered(); break;
+        case 5: _t->on_actionCopy_triggered(); break;
+        case 6: _t->on_actionPaste_triggered(); break;
+        case 7: _t->on_actionUndo_triggered(); break;
+        case 8: _t->on_actionRedo_triggered(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -66,7 +116,12 @@ const QMetaObject MainWindow::staticMetaObject = { {
     qt_meta_data_MainWindow,
     qt_static_metacall,
     nullptr,
-    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
+
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
     nullptr
 } };
 
@@ -87,6 +142,17 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 9;
+    }
     return _id;
 }
 QT_WARNING_POP
