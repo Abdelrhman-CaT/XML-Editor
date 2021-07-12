df60e43803ec806be153116391b4b1be11852f2d
Abdelrhman Hany <mindenslavement@gmail.com>
2021-07-12 18:37:16 +0200

    identify consistency errors TRIAL

--- XML_Editor/consistency_check.cpp
+++ XML_Editor/consistency_check.cpp
@@ -1,5 +1,7 @@
 #include "consistency_check.h"
 #include "QStack"
+#include "QMap"
+
 // XML Consistency Check. Returns a boolean value whether the file is XML-consistent or not
 bool check_consistency(QVector<QString> xml){
     QStack<QString> tag;
@@ -40,3 +42,46 @@ bool check_consistency(QVector<QString> xml){
         }
     return (tag.empty());
 }
+
+
+QMap<qint32, QString> identify_errors(QVector<QString> xml){
+    QMap<qint32, QString> res;
+    QStack<QString> tag;
+        for (int i = 0; i < xml.size(); i++) {
+            QString line = xml[i];
+            if (line[0] == '<') {
+                if (line[1] == '/') {
+                    if(!tag.empty()){
+                        if (line != ("</" + tag.top())) {
+                            // unmatched closing and opening tags
+                            res[i] = "</" + tag.top();
+                            tag.pop();
+                        }
+                        else {
+                            tag.pop();
+                        }
+                    }
+                    else{
+                        // closing tag without opening tag
+
+                    }
+                }
+                else if(line[1] != '!' && line[1] != '?') {
+                    if(!(line[line.length()-2] == '/' && line[line.length()-1] == '>')){
+                        QString temp = "";
+                        for(int j=1; j<line.length(); j++){
+                            if(line[j] != ' ' && line[j] != '>'){
+                             temp += line[j];
+                            }
+                            else{
+                                temp += '>';
+                                break;
+                            }
+                        }
+                        tag.push(temp);
+                    }
+                }
+            }
+        }
+}
+
